cb2a542eca12fa565fb6d4221b032778f2ae497d
prb01 <p.bergstroem@gmail.com>
2022-12-12 14:16:43 -0300

    updated to newest stable engine & improved prompt

--- pages/stability.js
+++ pages/stability.js
@@ -3,6 +3,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Stability() {
   const [imgSrc, setImgSrc] = useState("");
+  const [seed, setSeed] = useState("");
 
   const handleClick = async () => {
     try {
@@ -13,11 +14,12 @@ export default function Stability() {
         },
         body: JSON.stringify({
           prompt:
-            "Happy & smiling rabbit meets its soulmate. Colorful, illustration, insanely detailed, intricate, 8k, dramatic lighting, beautiful, epic composition, octane render",
+            "soulless Half-orc Sorcerer from the petrified wood who always wanted to learn magic but struggled with it. !!!!High fantasy art!!!!, full_body, full body, HDR, trending on artstation, !!!!glowing and epic!!!!, epic lighting from above, epic fantasy card game art, !!!!epic character portrait!!!!, full art illustration, !!!!senior character artist!!!! | blender, render, !!!text!!!, disfigured, cropped, out of frame, nsfw, grain:-3",
         }),
       });
       const { data } = await response.json();
       setImgSrc(data.imageBinary);
+      setSeed(data.seed);
     } catch (error) {
       console.log({ error });
     }
@@ -27,6 +29,7 @@ export default function Stability() {
     <div className={styles.container}>
       <button onClick={handleClick}>CLICK THIS</button>
       <img src={imgSrc} />
+      <span>{seed}</span>
     </div>
   );
 }
